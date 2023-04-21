3eb559af16d58944117a9459ef69ba768e633b57
fcamblor <fcamblor@gmail.com>
2023-04-22 00:13:55 +0200

    📗 added lit dependency + converted components to lit components

--- mobile/src/ts/capacitor-welcome.ts
+++ mobile/src/ts/capacitor-welcome.ts
@@ -1,143 +1,134 @@
 import { SplashScreen } from '@capacitor/splash-screen';
-import { Camera } from '@capacitor/camera';
+import {Camera, Photo} from '@capacitor/camera';
 import {CameraResultType} from "@capacitor/camera/dist/esm/definitions";
+import {css, html, LitElement} from "lit";
+import {customElement, query, state} from 'lit/decorators.js';
 
-window.customElements.define(
-  'capacitor-welcome',
-  class extends HTMLElement {
-    constructor() {
-      super();
+@customElement('capacitor-welcome')
+class CapacitorWelcome extends LitElement {
 
-      SplashScreen.hide();
+    @state()
+    private photo: Photo|undefined = undefined;
 
-      const root = this.attachShadow({ mode: 'open' });
+    static styles = [
+        css`
+  :host {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+  h1, h2, h3, h4, h5 {
+    text-transform: uppercase;
+  }
+  .button {
+    display: inline-block;
+    padding: 10px;
+    background-color: #73B5F6;
+    color: #fff;
+    font-size: 0.9em;
+    border: 0;
+    border-radius: 3px;
+    text-decoration: none;
+    cursor: pointer;
+  }
+  main {
+    padding: 15px;
+  }
+  main hr { height: 1px; background-color: #eee; border: 0; }
+  main h1 {
+    font-size: 1.4em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+  }
+  main h2 {
+    font-size: 1.1em;
+  }
+  main h3 {
+    font-size: 0.9em;
+  }
+  main p {
+    color: #333;
+  }
+  main pre {
+    white-space: pre-line;
+  }
+        `
+    ]
 
-      root.innerHTML = `
-    <style>
-      :host {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-        display: block;
-        width: 100%;
-        height: 100%;
-      }
-      h1, h2, h3, h4, h5 {
-        text-transform: uppercase;
-      }
-      .button {
-        display: inline-block;
-        padding: 10px;
-        background-color: #73B5F6;
-        color: #fff;
-        font-size: 0.9em;
-        border: 0;
-        border-radius: 3px;
-        text-decoration: none;
-        cursor: pointer;
-      }
-      main {
-        padding: 15px;
-      }
-      main hr { height: 1px; background-color: #eee; border: 0; }
-      main h1 {
-        font-size: 1.4em;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-      }
-      main h2 {
-        font-size: 1.1em;
-      }
-      main h3 {
-        font-size: 0.9em;
-      }
-      main p {
-        color: #333;
-      }
-      main pre {
-        white-space: pre-line;
-      }
-    </style>
-    <div>
-      <capacitor-welcome-titlebar>
-        <h1>Capacitor</h1>
-      </capacitor-welcome-titlebar>
-      <main>
-        <p>
-          Capacitor makes it easy to build powerful apps for the app stores, mobile web (Progressive Web Apps), and desktop, all
-          with a single code base.
-        </p>
-        <h2>Getting Started</h2>
-        <p>
-          You'll probably need a UI framework to build a full-featured app. Might we recommend
-          <a target="_blank" href="http://ionicframework.com/">Ionic</a>?
-        </p>
-        <p>
-          Visit <a href="https://capacitorjs.com">capacitorjs.com</a> for information
-          on using native features, building plugins, and more.
-        </p>
-        <a href="https://capacitorjs.com" target="_blank" class="button">Read more</a>
-        <h2>Tiny Demo</h2>
-        <p>
-          This demo shows how to call Capacitor plugins. Say cheese!
-        </p>
-        <p>
-          <button class="button" id="take-photo">Take Photo</button>
-        </p>
-        <p>
-          <img id="image" style="max-width: 100%">
-        </p>
-      </main>
-    </div>
-    `;
+    protected render(): unknown {
+        SplashScreen.hide();
+
+        return html`
+            <div>
+              <capacitor-welcome-titlebar>
+                <h1>Capacitor</h1>
+              </capacitor-welcome-titlebar>
+              <main>
+                <p>
+                  Capacitor makes it easy to build powerful apps for the app stores, mobile web (Progressive Web Apps), and desktop, all
+                  with a single code base.
+                </p>
+                <h2>Getting Started</h2>
+                <p>
+                  You'll probably need a UI framework to build a full-featured app. Might we recommend
+                  <a target="_blank" href="http://ionicframework.com/">Ionic</a>?
+                </p>
+                <p>
+                  Visit <a href="https://capacitorjs.com">capacitorjs.com</a> for information
+                  on using native features, building plugins, and more.
+                </p>
+                <a href="https://capacitorjs.com" target="_blank" class="button">Read more</a>
+                <h2>Tiny Demo</h2>
+                <p>
+                  This demo shows how to call Capacitor plugins. Say cheese!
+                </p>
+                <p>
+                  <button class="button" id="take-photo" @click="${event => this.takePhoto()}">Take Photo</button>
+                </p>
+                <p>
+                  <img id="image" style="max-width: 100%" src="${this.photo?.webPath}">
+                </p>
+              </main>
+            </div>
+        `
     }
 
-    connectedCallback() {
-      const self = this;
-
-      self.shadowRoot.querySelector('#take-photo').addEventListener('click', async function (e) {
+    private async takePhoto() {
         try {
-          const photo = await Camera.getPhoto({
-            resultType: CameraResultType.Uri,
-          });
-
-          const image = self.shadowRoot.querySelector<HTMLImageElement>('#image');
-          if (!image) {
-            return;
-          }
-
-          image.src = photo.webPath;
+            this.photo = await Camera.getPhoto({
+                resultType: CameraResultType.Uri,
+            });
         } catch (e) {
-          console.warn('User cancelled', e);
+            console.warn('User cancelled', e);
         }
-      });
     }
-  }
-);
+}
 
-window.customElements.define(
-  'capacitor-welcome-titlebar',
-  class extends HTMLElement {
-    constructor() {
-      super();
-      const root = this.attachShadow({ mode: 'open' });
-      root.innerHTML = `
-    <style>
-      :host {
-        position: relative;
-        display: block;
-        padding: 15px 15px 15px 15px;
-        text-align: center;
-        background-color: #73B5F6;
-      }
-      ::slotted(h1) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-        font-size: 0.9em;
-        font-weight: 600;
-        color: #fff;
-      }
-    </style>
-    <slot></slot>
-    `;
+@customElement('capacitor-welcome-titlebar')
+class CapacitorWelcomeTitlebar extends LitElement {
+    static styles = [
+        css`
+          :host {
+            position: relative;
+            display: block;
+            padding: 15px 15px 15px 15px;
+            text-align: center;
+            background-color: #73B5F6;
+          }
+          ::slotted(h1) {
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+            font-size: 0.9em;
+            font-weight: 600;
+            color: #fff;
+          }
+        `
+    ]
+
+    protected render(): unknown {
+        return html`
+          <slot></slot>
+        `
     }
-  }
-);
+}
