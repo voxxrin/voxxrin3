4adfa048efa6e8c700cf78b6edd8c9bf3e2d3c24
fcamblor <fcamblor@gmail.com>
2023-04-30 01:53:34 +0200

    🔧 added FAB (floating action button) on schedule screen
    this is only wip, I didn't succeeded to well display sub fab labels

--- mobile/src/views/event/SchedulePage.vue
+++ mobile/src/views/event/SchedulePage.vue
@@ -15,12 +15,37 @@
       <ion-button router-direction="forward" @click="router.push({name: 'eventTalkDetails', params: {eventId, talkId: '2'}})">
         Open talk 2
       </ion-button>
+
+      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+        <ion-fab-button>
+          <ion-icon :icon="chatbubble"></ion-icon>
+        </ion-fab-button>
+        <ion-fab-list side="top">
+          <ion-fab-button id="testing-alert1">
+            <ion-icon :icon="addCircle"></ion-icon>
+          </ion-fab-button>
+          <ion-label>test 1</ion-label>
+          <ion-fab-button id="testing-alert2">
+            <ion-icon :icon="addCircle"></ion-icon>
+          </ion-fab-button>
+          <ion-label>test 2</ion-label>
+        </ion-fab-list>
+        <ion-alert
+            trigger="testing-alert1" header="Alert"
+            subHeader="Testing message" message="This is alert 1 !"
+        ></ion-alert>
+        <ion-alert
+            trigger="testing-alert2" header="Alert"
+            subHeader="Testing message" message="This is alert 2 !"
+        ></ion-alert>
+      </ion-fab>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonFabButton, IonFab, IonAlert, IonLabel, IonFabList } from '@ionic/vue';
+import { chatbubble, addCircle } from 'ionicons/icons';
 import {useRoute, useRouter} from "vue-router";
 import {ref} from "vue";
 
@@ -28,3 +53,6 @@ const router = useRouter();
 const route = useRoute();
 const eventId = ref(route.params.eventId);
 </script>
+
+<style scoped>
+</style>
