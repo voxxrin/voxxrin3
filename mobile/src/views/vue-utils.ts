cc01814a4188acbc75ddc4c1aa3b4dd0535355f5
fcamblor <fcamblor@gmail.com>
2023-04-30 10:29:28 +0200

    added vue utilities to extract route params as a single value
    didn't find an exiting vue api for this (this surprised me)

---
+++ mobile/src/views/vue-utils.ts
@@ -0,0 +1,14 @@
+import {RouteLocationNormalizedLoaded} from "vue-router";
+
+// Ensure that we only get a single value from route params
+// this is intended to workaround the fact that route.params.foo is string|string[] and we want
+// to "flatten" this
+export const getRouteParamsValue = (route: RouteLocationNormalizedLoaded, paramName: string): string => {
+    let paramValue = route.params[paramName]
+
+    if (Array.isArray(paramValue) && paramValue.length) {
+        [paramValue] = paramValue
+    }
+
+    return paramValue as string;
+}
