bdb1cd178966cdd8a1db879e65634532263b7192
fcamblor <fcamblor@gmail.com>
2023-05-02 10:03:28 +0200

    added basic main.css file

--- mobile/src/main.ts
+++ mobile/src/main.ts
@@ -23,6 +23,8 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import './styles/main.css'
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
