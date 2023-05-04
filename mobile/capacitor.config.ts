8de2f2e542732e035447fdc0e024a89999f56ed1
fcamblor <fcamblor@gmail.com>
2023-05-04 22:11:13 +0200

    added capacitor android, ios and browser

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -3,8 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'voxxrin',
-  webDir: 'dist',
-  bundledWebRuntime: false
+  webDir: 'dist'
 };
 
 export default config;
