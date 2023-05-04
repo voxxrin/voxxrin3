4ad0cf98f81c88f62c8588280e21a4a83d153650
fcamblor <fcamblor@gmail.com>
2023-05-04 22:49:43 +0200

    added splash-screen capacitor plugin

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -3,7 +3,12 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.voxxrin3.mobile',
   appName: 'voxxrin',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0
+    }
+  }
 };
 
 export default config;
