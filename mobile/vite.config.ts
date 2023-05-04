252372416821d08d2d9dcd4d1c317489635fc98e
fcamblor <fcamblor@gmail.com>
2023-05-04 23:36:00 +0200

    generating sourcemaps in dev mode (+ added build-dev npm script)

--- mobile/vite.config.ts
+++ mobile/vite.config.ts
@@ -1,30 +1,32 @@
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { defineConfig } from 'vite'
+import {defineConfig, UserConfigExport} from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command, mode }) => {
+  const isDevMode = mode === 'dev'
+  const config: UserConfigExport = {
+    plugins: [
+      vue(),
+      legacy()
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  root: '.',
-  build: {
-    outDir: './dist',
-    minify: false,
-    emptyOutDir: true,
-  },
-  server: {
-    host: '0.0.0.0'
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  }
+    root: '.',
+    build: {
+      outDir: './dist',
+      minify: !isDevMode,
+      emptyOutDir: true,
+      sourcemap: isDevMode?'inline':false
+    },
+    server: {
+      host: '0.0.0.0'
+    }
+  };
+
+  return config;
 })
