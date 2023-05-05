3aad4ae1669b07a5c10bbb04f55324c8589a0890
Robin Lopez <robinlopez.contact@gmail.com>
2023-05-05 22:23:56 +0200

    (styles) - Change icons tabs bar actives

--- mobile/src/views/event/_BaseEventPages.vue
+++ mobile/src/views/event/_BaseEventPages.vue
@@ -3,23 +3,23 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="schedule" :href="`/events/${eventId.value}/schedule`">
-          <ion-icon aria-hidden="true" src="/assets/icons/line/calendar-line.svg"/>
+        <ion-tab-button tab="schedule" @click="selectedTab = 'schedule'" :href="`/events/${eventId.value}/schedule`">
+          <ion-icon aria-hidden="true" :src="selectedTab === 'schedule' ? scheduleSelectedIcon : scheduleIcon"/>
           <ion-label>Schedule</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="favorites" :href="`/events/${eventId.value}/favorites`">
-          <ion-icon aria-hidden="true" src="/assets/icons/line/bookmark-line-favorite.svg"/>
+        <ion-tab-button tab="favorites" @click="selectedTab = 'favorites'" :href="`/events/${eventId.value}/favorites`">
+          <ion-icon aria-hidden="true" :src="selectedTab === 'favorites' ? favoritesSelectedIcon : favoritesIcon"/>
           <ion-label>Favorites</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="feedbacks" :href="`/events/${eventId.value}/feedbacks`">
-          <ion-icon aria-hidden="true" src="/assets/icons/line/comments-2-line.svg"/>
+        <ion-tab-button tab="feedbacks" @click="selectedTab = 'feedbacks'" :href="`/events/${eventId.value}/feedbacks`">
+          <ion-icon aria-hidden="true" :src="selectedTab === 'feedbacks' ? feedbacksSelectedIcon : feedbacksIcon"/>
           <ion-label>Feedbacks</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="infos" :href="`/events/${eventId.value}/infos`">
-          <ion-icon aria-hidden="true" src="/assets/icons/line/info-circle-line.svg"/>
+        <ion-tab-button tab="infos" @click="selectedTab = 'infos'" :href="`/events/${eventId.value}/infos`">
+          <ion-icon aria-hidden="true" :src="selectedTab === 'infos' ? infosSelectedIcon : infosIcon"/>
           <ion-label>Infos</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -49,4 +49,16 @@ const route = useRoute();
 const eventId = ref(new EventId(getRouteParamsValue(route, 'eventId')!));
 const event = useCurrentConferenceDescriptor(eventId.value);
 
+
+const selectedTab = ref('schedule'); // Définit le tab initial sélectionné
+
+// Chemins des images pour chaque icône
+const scheduleIcon = '/assets/icons/line/calendar-line.svg';
+const scheduleSelectedIcon = '/assets/icons/solid/calendar.svg';
+const favoritesIcon = '/assets/icons/line/bookmark-line-favorite.svg';
+const favoritesSelectedIcon = '/assets/icons/solid/bookmark-favorite.svg';
+const feedbacksIcon = '/assets/icons/line/comments-2-line.svg';
+const feedbacksSelectedIcon = '/assets/icons/solid/comments-2.svg';
+const infosIcon = '/assets/icons/line/info-circle-line.svg';
+const infosSelectedIcon = '/assets/icons/solid/info-circle.svg';
 </script>
