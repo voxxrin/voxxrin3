19dcb7b56f2eac9a1aa959def4d69fc607717cc1
fcamblor <fcamblor@gmail.com>
2023-05-09 22:17:41 +0200

    introduced Range-based utility classes, allowing to calculate if some ranges overlap

--- mobile/src/models/utils.ts
+++ mobile/src/models/utils.ts
@@ -26,3 +26,32 @@ export function sortBy<T, H extends string|number>(arr: T[], hash: (val: T) => H
         }
     })
 }
+
+function overlap<R extends Range<T>, T>(r1: R, r2: R, comparator: (v1: T, v2: T) => number, type: 'inclusive'|'exclusive'): boolean {
+    return comparator(r1.start, r2.end) <= (type==='inclusive'?0:-1)
+        && comparator(r2.start, r1.end) <= (type==='inclusive'?0:-1);
+}
+export class Range<T> {
+    constructor(
+        public readonly start: T,
+        public readonly end: T
+    ) {}
+}
+export class NumberRange extends Range<number> {
+    constructor(start: number, end: number) {
+        super(start, end);
+    }
+
+    public static overlap(r1: NumberRange, r2: NumberRange, type: 'inclusive'|'exclusive'): boolean {
+        return overlap<NumberRange, number>(r1, r2, (v1, v2) => v1 - v2, type)
+    }
+}
+export class StringRange<S extends string = string> extends Range<S> {
+    constructor(start: S, end: S) {
+        super(start, end);
+    }
+
+    public static overlap<S extends string = string>(r1: StringRange<S>, r2: StringRange<S>, type: 'inclusive'|'exclusive'): boolean {
+        return overlap<StringRange<S>, S>(r1, r2, (v1, v2) => v1.localeCompare(v2), type)
+    }
+}
