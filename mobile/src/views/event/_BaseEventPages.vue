cfe4961b2314ca6595ba8c647a98f88bb66437ff
fcamblor <fcamblor@gmail.com>
2023-05-09 13:01:01 +0200

    refactored baseEventPages so that code is a little clearer

--- mobile/src/views/event/_BaseEventPages.vue
+++ mobile/src/views/event/_BaseEventPages.vue
@@ -3,24 +3,10 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="schedule" @click="selectedTab = 'schedule'" :href="`/events/${eventId.value}/schedule`">
-          <ion-icon aria-hidden="true" :src="selectedTab === 'schedule' ? scheduleSelectedIcon : scheduleIcon"/>
-          <ion-label>Schedule</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="favorites" @click="selectedTab = 'favorites'" :href="`/events/${eventId.value}/favorites`">
-          <ion-icon aria-hidden="true" :src="selectedTab === 'favorites' ? favoritesSelectedIcon : favoritesIcon"/>
-          <ion-label>Favorites</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="feedbacks" @click="selectedTab = 'feedbacks'" :href="`/events/${eventId.value}/feedbacks`">
-          <ion-icon aria-hidden="true" :src="selectedTab === 'feedbacks' ? feedbacksSelectedIcon : feedbacksIcon"/>
-          <ion-label>Feedbacks</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="infos" @click="selectedTab = 'infos'" :href="`/events/${eventId.value}/infos`">
-          <ion-icon aria-hidden="true" :src="selectedTab === 'infos' ? infosSelectedIcon : infosIcon"/>
-          <ion-label>Infos</ion-label>
+        <ion-tab-button v-for="(tab, index) in tabs" :key="index"
+            :tab="tab.id" @click="(ev: Event) => tabClicked(tab, ev)" :href="tab.url">
+          <ion-icon aria-hidden="true" :src="selectedTab === tab.id ? tab.selectedIcon : tab.icon"/>
+          <ion-label>{{ tab.label }}</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -35,30 +21,40 @@ import {
     IonLabel,
     IonIcon,
     IonPage,
-    IonRouterOutlet,
+    IonRouterOutlet, useIonRouter,
 } from '@ionic/vue';
-import {ref} from "vue";
-import {useRoute, useRouter} from "vue-router";
-import CurrentEventHeader from "@/components/CurrentEventHeader.vue";
+import {onMounted, ref} from "vue";
+import {useRoute} from "vue-router";
 import {getRouteParamsValue} from "@/views/vue-utils";
 import {EventId} from "@/models/VoxxrinEvent";
 import {useCurrentConferenceDescriptor} from "@/state/CurrentConferenceDescriptor";
 
-const router = useRouter();
+const router = useIonRouter();
 const route = useRoute();
 const eventId = ref(new EventId(getRouteParamsValue(route, 'eventId')!));
 const event = useCurrentConferenceDescriptor(eventId.value);
 
-
 const selectedTab = ref('schedule'); // Définit le tab initial sélectionné
 
-// Chemins des images pour chaque icône
-const scheduleIcon = '/assets/icons/line/calendar-line.svg';
-const scheduleSelectedIcon = '/assets/icons/solid/calendar.svg';
-const favoritesIcon = '/assets/icons/line/bookmark-line-favorite.svg';
-const favoritesSelectedIcon = '/assets/icons/solid/bookmark-favorite.svg';
-const feedbacksIcon = '/assets/icons/line/comments-2-line.svg';
-const feedbacksSelectedIcon = '/assets/icons/solid/comments-2.svg';
-const infosIcon = '/assets/icons/line/info-circle-line.svg';
-const infosSelectedIcon = '/assets/icons/solid/info-circle.svg';
+const tabs = [{
+  id: 'schedule', url: `/events/${eventId.value.value}/schedule`, label: 'Schedule',
+  icon: '/assets/icons/line/calendar-line.svg',
+  selectedIcon: '/assets/icons/solid/calendar.svg',
+}, {
+  id: 'favorites', url: `/events/${eventId.value.value}/favorites`, label: 'Favorites',
+  icon: '/assets/icons/line/bookmark-line-favorite.svg',
+  selectedIcon: '/assets/icons/solid/bookmark-favorite.svg',
+}, {
+  id: 'feedbacks', url: `/events/${eventId.value.value}/feedbacks`, label: 'Feedbacks',
+  icon: '/assets/icons/line/comments-2-line.svg',
+  selectedIcon: '/assets/icons/solid/comments-2.svg',
+}, {
+  id: 'infos', url: `/events/${eventId.value.value}/infos`, label: 'Infos',
+  icon: '/assets/icons/line/info-circle-line.svg',
+  selectedIcon: '/assets/icons/solid/info-circle.svg',
+}] as const;
+
+function tabClicked(tab: typeof tabs[number], event: Event) {
+    selectedTab.value = tab.id;
+}
 </script>
