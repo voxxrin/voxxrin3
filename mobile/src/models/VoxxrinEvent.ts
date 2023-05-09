f636554b6ab2af66fd9938de38e492c562917e18
fcamblor <fcamblor@gmail.com>
2023-05-10 01:18:25 +0200

    implemented event-list search + add/remove to/from favorites

--- mobile/src/models/VoxxrinEvent.ts
+++ mobile/src/models/VoxxrinEvent.ts
@@ -3,6 +3,7 @@ import {Replace} from "@/models/type-utils";
 import {ListableEvent} from "../../../shared/event-list.firestore";
 import {VoxxrinDay} from "@/models/VoxxrinDay";
 import {Temporal} from "temporal-polyfill";
+import {useCurrentClock} from "@/state/CurrentClock";
 
 export class EventId extends ValueObject<string>{ _eventIdClassDiscriminator!: never; }
 export type ListableVoxxrinEvent = Replace<ListableEvent, {
@@ -11,3 +12,31 @@ export type ListableVoxxrinEvent = Replace<ListableEvent, {
     start: Temporal.ZonedDateTime,
     end: Temporal.ZonedDateTime
 }>
+
+export function searchEvents(events: ListableVoxxrinEvent[], searchCriteria: { terms: string|undefined, includePastEvents: boolean}, favoritedIds: EventId[]) {
+    const filteredEvents = events.filter(event => {
+        if(searchCriteria.terms
+            && [
+                event.title,
+                event.location.country,
+                event.location.city,
+                event.keywords.join(", "),
+                event.description,
+            ].join(" ").toUpperCase().indexOf(searchCriteria.terms.toUpperCase()) === -1
+        ) {
+            return false;
+        }
+        if(!searchCriteria.includePastEvents
+            && event.end.add({days:1}).startOfDay().epochMilliseconds < useCurrentClock().zonedDateTimeISO().epochMilliseconds) {
+            return false;
+        }
+        return true;
+    });
+
+    const favoritedIdValues = favoritedIds.map(id => id.value);
+
+    return {
+        events: filteredEvents,
+        favorites: filteredEvents.filter(ev => favoritedIdValues.includes(ev.id.value))
+    }
+}
--- mobile/src/views/EventSelectorPage.vue
+++ mobile/src/views/EventSelectorPage.vue
@@ -6,15 +6,21 @@
       </ion-toolbar>
     </ion-header>
     <ion-content>
+      <ion-input label="Search a conference" label-placement="stacked" fill="outline"
+                 :debounce="300" placeholder="Keywords..."
+                 @ionInput="(ev) => searchTextUpdated(ev.target.value)"
+      ></ion-input>
+      <ion-toggle :enable-on-off-labels="true" @ionChange="(ev) => includePastEventUpdated(ev.target.checked)">Past events</ion-toggle>
+
       <h1>Favorited conferences</h1>
       <favorited-event-selector
-          :favoritedEvents="favoritedEvents" @event-selected="(event) => selectEvent(event.id)">
+          :favoritedEvents="filteredFavoritedEvents" @event-selected="(event) => selectEvent(event.id)">
         <template #no-favorites>
           No favorites available yet...
         </template>
       </favorited-event-selector>
       <h1>All conferences</h1>
-      <available-events-list :events="availableEvents" @event-clicked="(event) => showEventActions(event)">
+      <available-events-list :events="filteredAvailableEvents" @event-clicked="(event) => showEventActions(event)">
         <template #no-event>
           No conference registered yet
         </template>
@@ -25,12 +31,14 @@
 
 <script setup lang="ts">
 import {
+    IonToggle,
+    IonInput,
     useIonRouter
 } from '@ionic/vue';
-import {EventId, ListableVoxxrinEvent} from "@/models/VoxxrinEvent";
+import {EventId, ListableVoxxrinEvent, searchEvents} from "@/models/VoxxrinEvent";
 import {fetchConferenceDescriptor} from "@/state/CurrentConferenceDescriptor";
 import {fetchAvailableEvents, watchCurrentAvailableEvents} from "@/state/CurrentAvailableEvents";
-import {ref, Ref} from "vue";
+import {ref, Ref, watch} from "vue";
 import FavoritedEventSelector from "@/components/FavoritedEventSelector.vue";
 import AvailableEventsList from "@/components/AvailableEventsList.vue";
 import {presentActionSheetController} from "@/views/vue-utils";
@@ -38,14 +46,23 @@ import {Browser} from "@capacitor/browser";
 
 const router = useIonRouter();
 
-const favoritedEvents: Ref<ListableVoxxrinEvent[]> = ref([]);
-const availableEvents: Ref<ListableVoxxrinEvent[]> = ref([]);
+const availableEventsRef: Ref<ListableVoxxrinEvent[]> = ref([]);
 watchCurrentAvailableEvents(updatedAvailableEvents => {
-    availableEvents.value = updatedAvailableEvents;
-    favoritedEvents.value = updatedAvailableEvents;
+    availableEventsRef.value = updatedAvailableEvents;
 })
-
 fetchAvailableEvents();
+
+const favoritedTalksRef = ref<EventId[]>([]);
+
+const searchCriteriaRef = ref<{ terms: string|undefined, includePastEvents: boolean}>({ terms: undefined, includePastEvents: false });
+
+const filteredFavoritedEvents: Ref<ListableVoxxrinEvent[]> = ref([]);
+const filteredAvailableEvents: Ref<ListableVoxxrinEvent[]> = ref([]);
+watch([availableEventsRef, searchCriteriaRef, favoritedTalksRef], ([availableEvents, searchCriteria, favoritedTalks]) => {
+    const {events, favorites} = searchEvents(availableEvents, searchCriteria, favoritedTalks)
+    filteredAvailableEvents.value = events;
+    filteredFavoritedEvents.value = favorites;
+}, {immediate: true})
 
 async function selectEvent(eventId: EventId) {
     await fetchConferenceDescriptor(eventId);
@@ -53,26 +70,39 @@ async function selectEvent(eventId: EventId) {
     router.push(`/events/${eventId.value}`);
 }
 
+function searchTextUpdated(searchText: string) {
+    searchCriteriaRef.value = {...searchCriteriaRef.value, terms: searchText}
+}
+
+function includePastEventUpdated(includePastEvents: boolean) {
+    searchCriteriaRef.value = {...searchCriteriaRef.value, includePastEvents }
+}
+
 async function showEventActions(event: ListableVoxxrinEvent) {
     const result = await presentActionSheetController({
         header: 'Actions',
-        buttons: [{
+        buttons: [favoritedTalksRef.value.includes(event.id)?{
+            text: 'Remove from favorites',
+            data: {action: 'remove-from-favs'},
+        }:{
             text: 'Add to my favorites',
             data: {action: 'add-to-favs'},
-        }, {
+        }].concat(event.websiteUrl?[{
             text: 'Visit website',
             data: {action: 'visit-website'},
-        }, {
+        }]:[]).concat([{
             text: 'Cancel', role: 'cancel',
             data: {action: 'cancel'},
-        }].filter(btn => !!event.websiteUrl || btn.data?.action !== 'visit-website')
+        }])
     });
 
     // Not sure why, but ts-pattern's match() doesn't work here 🤔
     if(result?.action === 'visit-website') {
         Browser.open({url: event.websiteUrl})
     } else if(result?.action === 'add-to-favs') {
-        console.log(`TODO: Add to favorites !`)
+        favoritedTalksRef.value = [...favoritedTalksRef.value, event.id];
+    } else if(result?.action === 'remove-from-favs') {
+        favoritedTalksRef.value = favoritedTalksRef.value.filter(ev => !ev.isSameThan(event.id));
     } else if(result?.action === 'cancel') {
 
     } else {
