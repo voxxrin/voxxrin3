3a104539109b5000a1f92e44a8912d1f2c35eff2
Xavier Hanin <xavier.hanin@4sh.fr>
2023-05-13 16:27:47 +0200

    make use schedule more reactive

--- mobile/src/state/CurrentSchedule.ts
+++ mobile/src/state/CurrentSchedule.ts
@@ -1,53 +1,41 @@
-import {ref, watch} from "vue";
-import {
-    DailySchedule,
-} from "../../../shared/dayly-schedule.firestore";
+import {Ref, ref, watch} from "vue";
+import {DailySchedule} from "../../../shared/dayly-schedule.firestore";
 import {
     createVoxxrinDailyScheduleFromFirestore,
     VoxxrinDailySchedule
 } from "@/models/VoxxrinSchedule";
 import {DayId} from "@/models/VoxxrinDay";
 import {VoxxrinConferenceDescriptor} from "@/models/VoxxrinConferenceDescriptor";
-import { doc, onSnapshot } from "firebase/firestore";
-import { db } from "@/state/firebase";
+import {doc, onSnapshot} from "firebase/firestore";
+import {db} from "@/state/firebase";
 
 
 const CURRENT_SCHEDULE = ref<VoxxrinDailySchedule|undefined>(undefined);
-
 let clearFirestoreWatch = () => {}
-
-export const useCurrentSchedule = () => CURRENT_SCHEDULE.value;
-
-export const watchCurrentSchedule = (callback: (currentSchedule: (VoxxrinDailySchedule | undefined)) => void,) => {
-    watch(CURRENT_SCHEDULE, callback, {immediate: true});
-}
 
 export function unsetCurrentSchedule() {
     clearFirestoreWatch();
     CURRENT_SCHEDULE.value = undefined;
 }
 
-export const fetchSchedule = (conferenceDescriptor: VoxxrinConferenceDescriptor, dayId: DayId) => {
-    // Avoiding to fetch schedule if the one already loaded matches the one expected
-    if(
-        !CURRENT_SCHEDULE.value?.eventId.isSameThan(conferenceDescriptor.id)
-        || !CURRENT_SCHEDULE.value?.day.isSameThan(dayId)
-    ) {
+export const useSchedule = (conferenceDescriptor: Ref<VoxxrinConferenceDescriptor | undefined>, dayId: Ref<DayId | undefined>) => {
+    clearFirestoreWatch();
+    watch([conferenceDescriptor, dayId], () => {
         clearFirestoreWatch();
-        const d = doc(db, `events/${conferenceDescriptor.id.value}/days/${dayId.value}`)
-        console.debug(`subscribing to events/${conferenceDescriptor.id.value}/days/${dayId.value}`);
-        const unsubscribe = onSnapshot(d, docSnapshot => {
-                const firestoreDailySchedule: DailySchedule = docSnapshot.data() as DailySchedule
-                console.debug(
-                    `timeslots fetched from events/${conferenceDescriptor.id.value}/days/${dayId.value}:`, 
-                    firestoreDailySchedule.timeSlots);
-                CURRENT_SCHEDULE.value = createVoxxrinDailyScheduleFromFirestore(conferenceDescriptor, firestoreDailySchedule);
-            }, err => {
-                console.log(`Encountered error: ${err}`);
-            });
-        clearFirestoreWatch = () => {
-            console.debug(`unsubscribing from events/${conferenceDescriptor.id.value}/days/${dayId.value}`);
-            unsubscribe();
-        }    
-    }
+        if (conferenceDescriptor.value && dayId.value) {
+            const docPath = `events/${conferenceDescriptor.value.id.value}/days/${dayId.value.value}`
+            const d = doc(db, docPath)
+            clearFirestoreWatch = onSnapshot(d, docSnapshot => {
+                    const firestoreDailySchedule: DailySchedule = docSnapshot.data() as DailySchedule
+                    console.debug(`timeslots fetched from  ${docPath}:`, firestoreDailySchedule.timeSlots);
+                    CURRENT_SCHEDULE.value = 
+                        createVoxxrinDailyScheduleFromFirestore(conferenceDescriptor.value!, firestoreDailySchedule);
+                }, err => {
+                    console.log(`Encountered error: ${err}`);
+                });
+        } else {
+            CURRENT_SCHEDULE.value = undefined;
+        }
+    }, {immediate: true})
+    return CURRENT_SCHEDULE
 }
