39aab69f3403166e72bf170456032da035e5747c
fcamblor <fcamblor@gmail.com>
2023-05-15 19:35:03 +0200

    exported firebase config into .env files

--- mobile/src/state/firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyAW9jE7KrFM0FTzWa2CPl-NHQen-IwPKYs",
-    authDomain: "voxxrin-v3-poc.firebaseapp.com",
-    projectId: "voxxrin-v3-poc",
-    storageBucket: "voxxrin-v3-poc.appspot.com",
-    messagingSenderId: "20680838449",
-    appId: "1:20680838449:web:9049ca9161983d0b0d3410"
-};
--- mobile/src/state/firebase.ts
+++ mobile/src/state/firebase.ts
@@ -3,13 +3,20 @@ import {
     CACHE_SIZE_UNLIMITED,
     getFirestore,
     initializeFirestore,
+    connectFirestoreEmulator,
     persistentLocalCache,
     persistentMultipleTabManager,
 } from "firebase/firestore";
-import {config} from "@/state/firebase-config";
 
 
-export const app = initializeApp(config);
+export const app = initializeApp({
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+});
 
 initializeFirestore(app,
     {
@@ -21,3 +28,7 @@ initializeFirestore(app,
     });
 
 export const db = getFirestore(app);
+
+if (location.hostname === "localhost" && import.meta.env.DEV && import.meta.env.VITE_USE_LOCAL_FIREBASE_INSTANCE === 'true') {
+    connectFirestoreEmulator(db, 'localhost', 8080);
+}
