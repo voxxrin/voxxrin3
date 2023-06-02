7ca8b0a7fa535942a5d65dc3ba6d61c616d7b6e0
fcamblor <fcamblor@gmail.com>
2023-06-02 08:44:08 +0200

    moved feedback pages into dedicated /feedbacks directory

--- mobile/src/router/index.ts
+++ mobile/src/router/index.ts
@@ -13,8 +13,8 @@ const routes: Array<RouteRecordRaw> = [
     { path: 'notifications', component: () => import('@/views/event/NotificationsPage.vue') },
     { path: 'infos', component: () => import('@/views/event/InfosPage.vue') },
   ]},
-  { path: '/events/:eventId/new-feedback-for-timeslot/:timeslotId', component: () => import('@/views/event/NewFeedbackPage.vue') },
-  { path: '/events/:eventId/rate-talk/:talkId', component: () => import('@/views/RateTalkPage.vue') },
+  { path: '/events/:eventId/new-feedback-for-timeslot/:timeslotId', component: () => import('@/views/feedbacks/NewFeedbackPage.vue') },
+  { path: '/events/:eventId/rate-talk/:talkId', component: () => import('@/views/feedbacks/RateTalkPage.vue') },
   { path: '/events/:eventId/talks/:talkId/details', component: () => import('@/views/TalkDetailsPage.vue') },
 ]
 
