14265d298db352cc55dbd4eda7e7eebdfc16aab3
Robin Lopez <robinlopez.contact@gmail.com>
2023-06-06 14:46:19 +0200

    (styles) - Init skin ion modal

--- mobile/src/components/modals/SchedulePreferencesModal.vue
+++ mobile/src/components/modals/SchedulePreferencesModal.vue
@@ -1,15 +1,26 @@
 <template>
   <ion-header>
     <ion-toolbar>
-      <ion-buttons slot="start">
-        <ion-button color="medium" @click="close()">Close</ion-button>
-      </ion-buttons>
-      <ion-title>Modal Example</ion-title>
+      <div class="ion-modal-title">
+        <ion-icon src="/assets/icons/solid/filter.svg"></ion-icon>
+        <ion-title>Filters talks</ion-title>
+      </div>
+      <ion-button  @click="close()" slot="start" shape="round" size="default" fill="outline">
+        <ion-icon src="/assets/icons/line/close-line.svg"></ion-icon>
+      </ion-button>
     </ion-toolbar>
   </ion-header>
   <ion-content class="ion-padding">
     Hello world !
   </ion-content>
+  <ion-footer>
+    <ion-button slot="start" shape="round" size="default" fill="outline">
+      <ion-icon src="/assets/icons/line/reload-line.svg"></ion-icon>
+    </ion-button>
+    <ion-button slot="end" size="default">
+      Apply filter
+    </ion-button>
+  </ion-footer>
 </template>
 
 <script lang="ts">
