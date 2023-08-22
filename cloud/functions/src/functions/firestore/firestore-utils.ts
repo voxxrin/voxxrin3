fa60b620f1cda6f3c933db4ff988c68c3f3b5a87
fcamblor <fcamblor@gmail.com>
2023-08-22 15:14:05 +0200

    implemented attendeesFeedbacks endpoint

--- cloud/functions/src/functions/firestore/firestore-utils.ts
+++ cloud/functions/src/functions/firestore/firestore-utils.ts
@@ -1,4 +1,7 @@
 import {db} from "../../firebase";
+import {ConferenceOrganizerSpace} from "../../../../../shared/conference-organizer-space.firestore";
+import {UserFeedback} from "../../../../../shared/feedbacks.firestore";
+import {TalkAttendeeFeedback} from "../../../../../shared/talk-feedbacks.firestore";
 
 export async function getSecretTokenDoc<T>(path: string) {
     const list = await db.collection(path).listDocuments()
@@ -7,4 +10,30 @@ export async function getSecretTokenDoc<T>(path: string) {
     }
 
     return (await db.doc(`${path}/${list[0].id}`).get()).data() as T;
+}
+
+export async function ensureOrganizerTokenIsValid(eventId: string, organizerToken: string) {
+    const organizerSpace: ConferenceOrganizerSpace = await getSecretTokenDoc(`events/${eventId}/organizer-space`);
+
+    if(organizerSpace.organizerSecretToken !== organizerToken) {
+        throw new Error(`Invalid organizer token for eventId=${eventId}: ${organizerToken}`);
+    }
+
+    return organizerSpace;
+}
+
+export async function ensureTalkFeedbackViewerTokenIsValidThenGetFeedbacks(eventId: string, talkId: string, talkViewerToken: string) {
+    const talkFeedbacksViewerDoc = await db.doc(
+        `events/${eventId}/talks/${talkId}/feedbacks-access/${talkViewerToken}`
+    ).get()
+
+    if(!talkFeedbacksViewerDoc.exists) {
+        return [];
+    }
+
+    const feedbacksRefs = await talkFeedbacksViewerDoc.ref.collection('feedbacks').listDocuments()
+    const feedbackSnapshots = await Promise.all(feedbacksRefs.map(ref => ref.get()))
+    const feedbacks = feedbackSnapshots.map(snap => snap.data() as TalkAttendeeFeedback)
+
+    return feedbacks;
 }
--- cloud/functions/src/index.ts
+++ cloud/functions/src/index.ts
@@ -7,9 +7,11 @@ import {
     onTalkFeedbackCreated,
     onTalkFeedbackUpdated
 } from "./functions/firestore/onTalkFeedbackProvided";
+import attendeesFeedbacks from "./functions/http/event/attendeesFeedbacks";
 
 exports.helloWorld = hello
 exports.crawl = crawl
+exports.attendeesFeedbacks = attendeesFeedbacks
 exports.migrateFirestoreSchema = migrateFirestoreSchema
 
 exports.onUserTalksNoteCreate = onUserTalksNoteCreate
