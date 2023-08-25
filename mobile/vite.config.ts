402b99e648cf1ffb69c306524184f6bbe514975f
fcamblor <fcamblor@gmail.com>
2023-08-25 16:16:40 +0200

    externalized pwa manifest config into a dedicated json file, which will be easier to replace during whitelabel replacements

--- mobile/vite.config.ts
+++ mobile/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 import {defineConfig, UserConfigExport} from 'vite'
+import manifestConfig from './manifest-config.json'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -29,27 +30,7 @@ export default defineConfig(({ command, mode }) => {
           sourcemap: isDevMode,
           swDest: 'sw.js',
         },
-        manifest: {
-          name: "Voxxrin - Your conference companion",
-          short_name: "Voxxrin",
-          start_url: "index.html",
-          display: "standalone",
-          icons: [{
-            src: "assets/imgs/logo512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }, {
-            src: "assets/imgs/logo256.png",
-            sizes: "256x256",
-            type: "image/png"
-          }, {
-            src: "assets/imgs/logo128.png",
-            sizes: "128x128",
-            type: "image/png"
-          }],
-          background_color: "#E30A63",
-          theme_color: "transparent"
-        }
+        manifest: manifestConfig
       })
     ],
     resolve: {
