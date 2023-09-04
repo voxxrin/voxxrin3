c2af8774d917db6df96e9ce5c4b5db1c015212a8
fcamblor <fcamblor@gmail.com>
2023-09-04 03:47:18 +0200

    trying service worker implementation for PUSH Notification

--- mobile/src/sw.ts
+++ mobile/src/sw.ts
@@ -9,6 +9,48 @@ import {NetworkOnly, StaleWhileRevalidate, Strategy, StrategyHandler} from 'work
 import { ExpirationPlugin } from 'workbox-expiration';
 import {cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute} from "workbox-precaching";
 
+// 1/ Nothing
+// => RUNTIME: firebase is not defined
+
+// 2/
+// import 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js';
+// import 'https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js';
+// => RUNTIME: Uncaught SyntaxError: Cannot use import statement outside a module (at sw.js:1:1)
+
+// 3/
+// import "firebase/firebase-app-compat"
+// import "firebase/firebase-messaging-compat"
+// => BUILD TIME: Missing "./firebase-app-compat" specifier in "firebase" package
+
+// 4/
+// import "firebase/compat/app"
+// import "firebase/compat/messaging"
+// => RUNTIME: firebase is not defined
+
+// 5/
+// import "firebase/app"
+// import "firebase/messaging"
+// => RUNTIME: firebase is not defined
+
+// 6/
+// import * as firebase from "firebase/app";
+// import "firebase/messaging";
+// => RUNTIME execution error on messaging() row
+
+// 7/
+// const firebaseImportedPromise = Promise.resolve().then(() => Promise.all([
+//     // @ts-ignore
+//     import('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'),
+//     // @ts-ignore
+//     import('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js'),
+// ]))
+// => RUNTIME: TypeError: import() is disallowed on ServiceWorkerGlobalScope by the HTML specification. See https://github.com/w3c/ServiceWorker/issues/1356.
+
+// 8/
+import {initializeApp} from "firebase/app";
+import {getMessaging, onMessage} from "firebase/messaging";
+import {onBackgroundMessage} from "firebase/messaging/sw";
+
 declare let self: ServiceWorkerGlobalScope & {__WB_DISABLE_DEV_LOGS?: boolean}
 
 self.addEventListener('message', (event) => {
@@ -66,3 +108,25 @@ setCatchHandler(async (event) => {
     })}`)
     return Response.error();
 })
+
+// declare let firebase: any;
+const firebaseApp = initializeApp({
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+});
+const messaging = getMessaging(firebaseApp);
+
+onMessage(messaging, (payload: any) => {
+    console.log('Message received. ', payload);
+});
+onBackgroundMessage(messaging, (payload: any) => {
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
+        icon: '/assets/imgs/logo.png'
+    });
+});
