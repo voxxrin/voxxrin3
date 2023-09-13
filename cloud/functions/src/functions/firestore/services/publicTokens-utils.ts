24559483eaf04d3040a2a4163ca3fa1c5887381f
fcamblor <fcamblor@gmail.com>
2023-09-14 00:11:46 +0200

    #36 allowing to pass family-based token on /talkFeedbacksViewers endpoint

--- cloud/functions/src/functions/firestore/services/publicTokens-utils.ts
+++ cloud/functions/src/functions/firestore/services/publicTokens-utils.ts
@@ -1,23 +1,42 @@
 import {db} from "../../../firebase";
-import {FamilyEventsStatsAccessToken, PublicToken} from "../../../../../../shared/public-tokens";
+import {
+    FamilyEventsFeedbacksAccessToken,
+    FamilyEventsStatsAccessToken,
+    PublicToken
+} from "../../../../../../shared/public-tokens";
+import {match, P} from "ts-pattern";
 
 
 export function isFamilyEventsStats(publicToken: PublicToken): publicToken is FamilyEventsStatsAccessToken {
     return publicToken.type === 'FamilyEventsStatsAccess';
 }
+export function isFamilyEventsFeedbacks(publicToken: PublicToken): publicToken is FamilyEventsFeedbacksAccessToken {
+    return publicToken.type === 'FamilyEventsFeedbacksAccess';
+}
 
-export async function getFamilyEventsStatsToken(token: string) {
-    const publicTokenDoc = await db.doc(`/public-tokens/${token}`).get();
+async function getPublicTokenBySecret<T>(secretToken: string, transformer: (publicToken: PublicToken) => T|undefined, expectedTokenName: string) {
+    const publicTokenDoc = await db.doc(`/public-tokens/${secretToken}`).get();
 
     if(!publicTokenDoc.exists) {
-        throw new Error(`Invalid family events stats token: ${token}`);
+        throw new Error(`Invalid family events stats token: ${secretToken}`);
     }
 
     const publicTokenSnap = publicTokenDoc.data() as PublicToken;
 
-    if(!isFamilyEventsStats(publicTokenSnap)) {
-        throw new Error(`Expectation mismatch: public token ${token} is not a family events stats token !`)
-    }
+    return match(transformer(publicTokenSnap))
+        .with(P.nullish, () => {
+            throw new Error(`Expectation mismatch: public token ${secretToken} is not a ${expectedTokenName} !`)
+        }).otherwise((result: T) => result);
+}
 
-    return publicTokenSnap;
+export async function getFamilyEventsStatsToken(secretToken: string) {
+    return getPublicTokenBySecret(secretToken,
+            publicToken => isFamilyEventsStats(publicToken)?publicToken:undefined,
+        "family events stats token")
+}
+
+export async function getFamilyEventsFeedbacksToken(secretToken: string) {
+    return getPublicTokenBySecret(secretToken,
+            publicToken => isFamilyEventsFeedbacks(publicToken)?publicToken:undefined,
+        "family events feedbacks token")
 }
