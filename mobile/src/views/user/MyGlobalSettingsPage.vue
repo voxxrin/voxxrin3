208c7f638d7cbcdf482d8425820fa824dd2bad53
Robin Lopez <robinlopez.contact@gmail.com>
2023-09-18 11:15:04 +0200

    (styles) - Add theme selector

--- mobile/src/views/user/MyGlobalSettingsPage.vue
+++ mobile/src/views/user/MyGlobalSettingsPage.vue
@@ -10,6 +10,30 @@
           <ion-title class="stickyHeader-title" slot="start">App settings</ion-title>
         </ion-toolbar>
       </ion-header>
+
+      <vox-bar-section>
+        <template #title>Appearance</template>
+      </vox-bar-section>
+      <div class="sectionContainer">
+        <ion-text color="primary" class="sectionContainer-title">
+          <strong>Theme</strong>
+        </ion-text>
+        <ion-segment value="heart" class="classicSegmentButton">
+          <ion-segment-button value="call" layout="icon-start">
+            <ion-label>Light</ion-label>
+            <ion-icon  src="/assets/icons/line/lightbulb-shine-line.svg"></ion-icon>
+          </ion-segment-button>
+          <ion-segment-button value="heart" layout="icon-start">
+            <ion-label>Dark</ion-label>
+            <ion-icon  src="/assets/icons/solid/moon.svg"></ion-icon>
+          </ion-segment-button>
+          <ion-segment-button value="pin" layout="icon-start">
+            <ion-label>Auto</ion-label>
+            <ion-icon  src="/assets/icons/solid/contrast.svg"></ion-icon>
+          </ion-segment-button>
+        </ion-segment>
+
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -17,9 +41,11 @@
 <script setup lang="ts">
 import {goBackOrNavigateTo} from "@/router";
 import {useIonRouter} from "@ionic/vue";
+import VoxBarSection from "@/components/ui/VoxBarSection.vue";
 
 const ionRouter = useIonRouter();
 </script>
 
 <style lang="scss" scoped>
+
 </style>
