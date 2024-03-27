b7a20728dc2e004831f8ee25d8044839f60f6e0f
Robin Lopez <robinlopez.contact@gmail.com>
2024-03-27 15:47:46 +0100

    #74 (style) - Adjust SpeakerCard

--- mobile/src/components/speaker-card/SpeakerCard.vue
+++ mobile/src/components/speaker-card/SpeakerCard.vue
@@ -1,7 +1,6 @@
 <template>
   <!-- TODO Dev Card Speaker List -->
-  <ion-card class="speakerCard"
-            :class="{ '_is-highlighted': true, '_has-liked': true}">
+  <ion-card class="speakerCard">
     <div class="speakerCard-head">
       <div class="avatarContainer">
         <ion-thumbnail class="avatar _large">
@@ -21,14 +20,15 @@
     <div class="speakerCard-content">
       <ion-list class="talkResumeList">
         <SpeakerResumeTalk></SpeakerResumeTalk>
+        <SpeakerResumeTalk></SpeakerResumeTalk>
       </ion-list>
-    </div>
-    <div class="speakerCard-footer">
-      <div class="speakerActions">
-        <ion-button class="btnActionCard" :aria-label="LL.View_Profil_Speaker()">
-          <ion-icon  aria-hidden="true" src="/assets/icons/solid/info-circle.svg"></ion-icon>
-        </ion-button>
-        <SpeakerLikeButton></SpeakerLikeButton>
+      <div class="bulletTagList" role="list">
+        <div class="bulletTag" role="listitem">
+          <span class="bulletTag-nb">2</span>Conf.
+        </div>
+        <div class="bulletTag" role="listitem">
+          <span class="bulletTag-nb">1</span>TiA.
+        </div>
       </div>
     </div>
   </ion-card>
@@ -46,6 +46,7 @@
   import {businessSharp} from "ionicons/icons";
   import SpeakerLikeButton from "@/components/speaker-card/SpeakerLikeButton.vue";
   import SpeakerResumeTalk from "@/components/speaker-card/SpeakerResumeTalk.vue";
+  import SpeakerInfoButton from "@/components/speaker-card/SpeakerInfoButton.vue";
 
   const { LL } = typesafeI18n()
 
@@ -57,244 +58,158 @@
 
 <style lang="scss">
   .speakerCard {
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  margin: 8px;
-  border-radius: var(--app-card-radius);
-  border: {
-    top: 1px solid var(--app-grey-line);
-    right: 1px solid var(--app-grey-line);
-  }
-  box-shadow: var(--app-shadow-default);
-  transition: 80ms ease-in-out;
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+      margin: var(--app-gutters-medium) var(--app-gutters-small);
+      border-radius: var(--app-card-radius);
+      border: {
+        top: 1px solid var(--app-grey-line);
+        right: 1px solid var(--app-grey-line);
+      }
+      box-shadow: var(--app-shadow-default);
+      transition: 80ms ease-in-out;
 
-  @media (prefers-color-scheme: dark) {
-    background: var(--app-light-contrast);
-    border : {
-      top: 1px solid var(--app-line-contrast);
-      right: 1px solid var(--app-line-contrast);
-    }
-  }
+      @media (prefers-color-scheme: dark) {
+        background: var(--app-light-contrast);
+        border : {
+          top: 1px solid var(--app-line-contrast);
+          right: 1px solid var(--app-line-contrast);
+        }
+      }
 
-  //* States card talk *//
-  &:active {
-    transition: 80ms ease-in-out;
-    transform: scale(0.99);
-    box-shadow: rgba(99, 99, 99, 0.2) 0 2px 8px 0;
-  }
-
-  &:before, &:after {
-    position: absolute;
-    content: '';
-    z-index: -1;
-  }
-
-  &._is-highlighted {
-    border : {
-      top: 2px solid var(--app-primary);
-      bottom: 2px solid var(--app-primary);
-      right: 2px solid var(--app-primary);
-      left: 2px solid var(--app-primary);
+    //* States card talk *//
+    &:active {
+      transition: 80ms ease-in-out;
+      transform: scale(0.99);
+      box-shadow: rgba(99, 99, 99, 0.2) 0 2px 8px 0;
     }
 
-    @media (prefers-color-scheme: dark) {
+    &:before, &:after {
+      position: absolute;
+      content: '';
+      z-index: -1;
+    }
+
+    &._is-highlighted {
       border : {
-        top: 2px solid var(--app-white) !important;
-        bottom: 2px solid var(--app-white) !important;
-        right: 2px solid var(--app-white) !important;
-        left: 2px solid var(--app-white) !important;
+        top: 2px solid var(--app-primary);
+        bottom: 2px solid var(--app-primary);
+        right: 2px solid var(--app-primary);
+        left: 2px solid var(--app-primary);
       }
-    }
-
-    ion-thumbnail {
-      background-color: var(--app-background);
-      border: 2px solid var(--app-primary);
 
       @media (prefers-color-scheme: dark) {
-        border: 2px solid var(--app-white) !important;
+        border : {
+          top: 2px solid var(--app-white) !important;
+          bottom: 2px solid var(--app-white) !important;
+          right: 2px solid var(--app-white) !important;
+          left: 2px solid var(--app-white) !important;
+        }
       }
-    }
 
-    .speakerCard-footer {
-      border-width: 2px;
-      border-color: var(--app-primary);
-      border-bottom: none;
+      ion-thumbnail {
+        background-color: var(--app-background);
+        border: 2px solid var(--app-primary);
 
-      @media (prefers-color-scheme: dark) {
-        border-color: var(--app-white) !important;
+        @media (prefers-color-scheme: dark) {
+          border: 2px solid var(--app-white) !important;
+        }
+      }
+
+      &._has-liked {
+        border : {
+          top: 2px solid var(--app-primary-shade);
+          bottom: 2px solid var(--app-primary-shade);
+          right: 2px solid var(--app-primary-shade);
+        }
+
+        @media (prefers-color-scheme: dark) {
+          --border : {
+            top: 2px solid var(--app-white);
+            bottom: 2px solid var(--app-white);
+            right: 2px solid var(--app-white);
+          }
+        }
+
+        .btnActionCard {
+          border-color: var(--app-primary-shade);
+          border-width: 2px;
+
+          @media (prefers-color-scheme: dark) {
+            border-color: var(--app-white);
+          }
+        }
+
+        &:before { background: rgba(var(--voxxrin-event-theme-colors-primary-rgb), 0.6);}
+
+        ion-thumbnail { border: 2px solid var(--app-primary-shade);}
       }
     }
 
     &._has-liked {
-      border : {
-        top: 2px solid var(--app-primary-shade);
-        bottom: 2px solid var(--app-primary-shade);
-        right: 2px solid var(--app-primary-shade);
+      &:before {
+        width: 40%;
+        height: 50%;
+        right: 0;
+        bottom: 0;
+        transform: scale(1);
+        background: linear-gradient(331deg, rgba(var(--voxxrin-event-theme-colors-primary-rgb), 0.4) 30%, rgba(var(--voxxrin-event-theme-colors-primary-rgb), 0.4) 80%);
+        opacity: 1;
+        filter: blur(32px);
+        animation: scale-in-center 0.1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
       }
 
-      @media (prefers-color-scheme: dark) {
-        --border : {
-          top: 2px solid var(--app-white);
-          bottom: 2px solid var(--app-white);
-          right: 2px solid var(--app-white);
-        }
-      }
-
-      .btnActionCard {
-        border-color: var(--app-primary-shade);
-        border-width: 2px;
-
-        @media (prefers-color-scheme: dark) {
-          border-color: var(--app-white);
-        }
-      }
-
-      &:before { background: rgba(var(--voxxrin-event-theme-colors-primary-rgb), 0.6);}
-
-      ion-thumbnail { border: 2px solid var(--app-primary-shade);}
-
-      .talkCard-footer {
-        border-color: var(--app-primary-shade);
-      }
-    }
-  }
-
-  &._has-liked {
-    &:before {
-      width: 40%;
-      height: 50%;
-      right: 0;
-      bottom: 0;
-      transform: scale(1);
-      background: linear-gradient(331deg, rgba(var(--voxxrin-event-theme-colors-primary-rgb), 0.4) 30%, rgba(var(--voxxrin-event-theme-colors-primary-rgb), 0.4) 80%);
-      opacity: 1;
-      filter: blur(32px);
-      animation: scale-in-center 0.1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-    }
-
-    &:after {
-      width: 50%;
-      height: 100%;
-      right: 0;
-      bottom: 0;
-      background-image: url('/assets/images/png/texture-favorited.png');
-      background-repeat: no-repeat;
-      background-position: right;
-      background-size: cover;
-      transform: scale(1);
-      opacity: 1;
-      mix-blend-mode: overlay;
-      animation: scale-in-center 0.1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-
-      @media (prefers-color-scheme: dark) {
-        mix-blend-mode: difference;
-      }
-    }
-  }
-
-  &-head {
-    display: flex;
-    align-items: center;
-    gap: var(--app-gutters-medium);
-    padding: var(--app-gutters) var(--app-gutters) var(--app-gutters-small) var(--app-gutters);
-  }
-
-  &-content {
-    display: flex;
-    column-gap: var(--app-gutters);
-    justify-content: space-between;
-    padding: 0 var(--app-gutters) var(--app-gutters-small) var(--app-gutters);
-
-
-    .talkResumeList {
-      background: transparent;
-      width: 100%;
-    }
-
-    .talkResumeCard {
-      --padding-start: 0;
-      --inner-padding-top: var(--app-gutters-medium);
-      --inner-padding-bottom: var(--app-gutters-medium);
-      --background: transparent;
-      padding: 0;
-
-      &:last-child {
-        --border-style: none;
-        --inner-padding-bottom: 0;
-      }
-
-      &-line {
+      &:after {
+        width: 50%;
         height: 100%;
-        min-width: 2px;
-        margin-right: var(--app-gutters-medium);
-        border-radius: 4px;
-        background: var(--app-primary);
+        right: 0;
+        bottom: 0;
+        background-image: url('/assets/images/png/texture-favorited.png');
+        background-repeat: no-repeat;
+        background-position: right;
+        background-size: cover;
+        transform: scale(1);
+        opacity: 1;
+        mix-blend-mode: overlay;
+        animation: scale-in-center 0.1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
 
         @media (prefers-color-scheme: dark) {
-          background: var(--app-white-90);
-        }
-      }
-
-      &-content {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: var(--app-gutters-medium);
-
-        &-description {
-          width: 100%;
-          font-size: 14px;
-          line-height: 1.2;
-          color: var(--app-primary);
-
-          @media (prefers-color-scheme: dark) {
-            color: var(--app-white);
-          }
-        }
-      }
-
-      &-footer {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        width: 100%;
-
-        .avatarContainer {
-          gap: var(--app-gutters);
-        }
-
-        .avatarInfos {
-          justify-content: end;
+          mix-blend-mode: difference;
         }
       }
     }
-  }
 
-  &-footer {
-    display: flex;
-    column-gap: 16px;
-    justify-content: end;
-    border-radius: 0 0 12px 0;
-    border : {
-      top: 1px solid var(--app-grey-line);
-      bottom: 1px solid var(--app-grey-line);
-    }
-    background-color: rgba(white, 0.6);
-
-    @media (prefers-color-scheme: dark) {
-      background-color: rgba(white, 0.05);
-      border : {
-        top: 1px solid var(--app-line-contrast);
-        bottom: 1px solid var(--app-line-contrast);
-      }
-    }
-
-    .speakerActions {
+    &-head {
       display: flex;
-      flex-direction: row;
+      align-items: center;
+      gap: var(--app-gutters-medium);
+      padding: var(--app-gutters-medium) var(--app-gutters-medium) var(--app-gutters-medium) 72px;
+
+      .avatar {
+        position: absolute;
+        left: -18px;
+        top: -8px;
+        --size: 74px;
+      }
+    }
+
+    &-content {
+      display: flex;
+      flex-direction: column;
+      gap: var(--app-gutters);
+      justify-content: space-between;
+      padding-bottom: var(--app-gutters-medium);
+
+      .talkResumeList {
+        background: transparent;
+        width: 100%;
+        overflow: visible;
+      }
+
+      .bulletTagList {
+        padding-left: 54px;
+      }
     }
   }
-}
 </style>
--- mobile/src/components/speaker-card/SpeakerResumeTalk.vue
+++ mobile/src/components/speaker-card/SpeakerResumeTalk.vue
@@ -1,29 +1,37 @@
 <template>
   <!-- TODO Dev Resume Card -->
-  <ion-item class="talkResumeCard">
+  <ion-item class="talkResumeCard" :class="{'_has-favorited': false}">
     <span class="talkResumeCard-line"></span>
     <div class="talkResumeCard-content">
-      <ion-text class="talkResumeCard-content-description">
-        Unlocking the Secrets of the Devoxx Mobile App: A Deep Dive into Open Source PWA with Vue and Firebase
-      </ion-text>
+      <div class="talkResumeCard-content-description">
+        <ion-text>
+          Unlocking the Secrets of the Devoxx Mobile App: A Deep Dive into Open Source PWA with Vue and Firebase
+        </ion-text>
+        <SpeakerFavTalkButton></SpeakerFavTalkButton>
+      </div>
       <div class="talkResumeCard-footer">
-        <ion-badge class="trackBadge">
-          <div class="trackBadge-content">
-            <ion-icon src="/assets/icons/solid/tag.svg"></ion-icon>JAVA
-          </div>
-        </ion-badge>
+        <div class="talkResumeCard-footer-left">
+          <ion-badge class="trackBadge">
+            <div class="trackBadge-content">
+              <ion-icon src="/assets/icons/solid/tag.svg"></ion-icon>
+              JAVA
+            </div>
+          </ion-badge>
+          <div class="bulletTag _labelOnly" role="listitem">Conf.</div>
+        </div>
+
         <div class="avatarContainer">
           <div class="avatarGroup" v-if="true">
             <div class="avatarItem">
               <ion-thumbnail class="avatar _small">
-                <img v-if="false" :src="speaker.photoUrl" @error="handle404OnSpeakerThumbnail($event.target as HTMLImageElement)"
+                <img v-if="false" :src="speaker.photoUrl"
+                     @error="handle404OnSpeakerThumbnail($event.target as HTMLImageElement)"
                      :alt="LL.Avatar_Speaker() + ' ' + speaker.fullName"/>
-                <img v-if="true" aria-hidden="true" :src="baseUrl+'assets/images/svg/avatar-shadow.svg'" />
+                <img v-if="true" aria-hidden="true" :src="baseUrl+'assets/images/svg/avatar-shadow.svg'"/>
               </ion-thumbnail>
             </div>
           </div>
           <div class="avatarInfos _small">
-            <ion-text class="avatarInfos-title">Conference</ion-text>
             <ion-text class="avatarInfos-subTitle" v-if="true">
               (+X Speaker(s))
             </ion-text>
@@ -37,20 +45,64 @@
 <script setup lang="ts">
 import {typesafeI18n} from "@/i18n/i18n-vue";
 import {IonBadge, IonThumbnail} from "@ionic/vue";
-const { LL } = typesafeI18n()
+import SpeakerFavTalkButton from "@/components/speaker-card/SpeakerFavTalkButton.vue";
+
+const {LL} = typesafeI18n()
 const baseUrl = import.meta.env.BASE_URL;
 </script>
 
 <style lang="scss" scoped>
-  .talkResumeCard {
-  --padding-start: 0;
+.talkResumeCard {
+  --background: transparent;
+  --inner-padding-end: 0;
   --inner-padding-top: var(--app-gutters-medium);
   --inner-padding-bottom: var(--app-gutters-medium);
-  padding: 0;
+  overflow: visible;
 
   &:last-child {
     --border-style: none;
     --inner-padding-bottom: 0;
+  }
+
+  &._has-favorited {
+
+    .talkResumeCard-line {
+      background: var(--voxxrin-event-theme-colors-primary-hex);
+    }
+
+    .talkResumeCard-content {
+      &:before {
+        width: 84px;
+        height: 60%;
+        right: -44px;
+        top: 16px;
+        transform: scale(1);
+        background: linear-gradient(331deg, rgba(var(--voxxrin-event-theme-colors-primary-rgb), 0.8) 30%,
+            rgba(var(--voxxrin-event-theme-colors-primary-rgb), 0.8) 80%);
+        opacity: 1;
+        filter: blur(24px);
+        animation: scale-in-center 0.1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+      }
+
+      &:after {
+        width: 84px;
+        height: 60%;
+        right: -44px;
+        top: 16px;
+        background-image: url('/assets/images/png/texture-favorited.png');
+        background-repeat: no-repeat;
+        background-position: right;
+        background-size: cover;
+        transform: scale(1) translate(0, -50%);;
+        opacity: 0.5;
+        mix-blend-mode: overlay;
+        animation: scale-in-center 0.1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+
+        @media (prefers-color-scheme: dark) {
+          mix-blend-mode: difference;
+        }
+      }
+    }
   }
 
   &-line {
@@ -62,17 +114,42 @@ const baseUrl = import.meta.env.BASE_URL;
   }
 
   &-content {
+    position: relative;
     flex: 1;
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: var(--app-gutters-medium);
+    gap: var(--app-gutters-small);
+    padding-right: var(--app-gutters-medium);
+    background: var(--app-white);
+
+    @media (prefers-color-scheme: dark) {
+      background: transparent;
+    }
+
+    &:before, &:after {
+      position: absolute;
+      content: '';
+      z-index: 0;
+      pointer-events: none;
+    }
 
     &-description {
+      display: flex;
       width: 100%;
       font-size: 14px;
       line-height: 1.2;
       color: var(--app-primary);
+
+      ion-text {
+        flex: 1;
+        line-height: 1.4;
+        padding-right: var(--app-gutters-small);
+
+        @media (prefers-color-scheme: dark) {
+          color: var(--app-white);
+        }
+      }
     }
   }
 
@@ -82,6 +159,12 @@ const baseUrl = import.meta.env.BASE_URL;
     justify-content: space-between;
     width: 100%;
 
+    &-left {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: var(--app-gutters-medium)
+    }
 
     .avatarContainer {
       gap: var(--app-gutters);
