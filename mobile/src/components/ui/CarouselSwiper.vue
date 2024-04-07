e9d44ba95fb4b9a123125bd32497cfacd3b98745
Robin Lopez <robinlopez.contact@gmail.com>
2024-04-07 19:39:46 +0200

    (style) - Adjust style for CarouselSwiper

--- mobile/src/components/ui/CarouselSwiper.vue
+++ mobile/src/components/ui/CarouselSwiper.vue
@@ -33,8 +33,15 @@ const props = defineProps({
 .swiper {
   width: 100%;
   height: 500px;
+  padding-bottom: 38px;
   border-radius: 12px;
+  border: 1px solid var(--app-beige-line);
+  background: var(--app-grey-light);
   box-shadow: var(--app-shadow-light);
+
+  @media (prefers-color-scheme: dark) {
+    background: var(--app-dark-contrast);
+  }
 
   .callout {
     position: absolute;
@@ -42,13 +49,24 @@ const props = defineProps({
   }
 
   .swiper-zoom-container {
-    border-radius: 12px;
-    background: var(--app-white);
-    border: 1px solid var(--app-beige-line);
+    border-radius: 12px 12px 0 0;
+  }
+}
 
-    @media (prefers-color-scheme: dark) {
-      background: var(--app-dark-contrast);
-    }
+.swiper-pagination {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  bottom: 0 !important;
+  height: 38px;
+  background: var(--app-white);
+  box-shadow: rgba(0, 0, 0, 0.24) 0 3px 8px;
+  border: none !important;
+  z-index: 0;
+
+  @media (prefers-color-scheme: dark) {
+    background: var(--app-light-contrast);
+    border: 4px solid var(--app-dark-contrast);
   }
 }
 
@@ -58,15 +76,20 @@ const props = defineProps({
   display: flex;
   flex-direction: column;
   justify-content: center;
-  height: 44px;
-  width: 44px;
+  height: 48px;
+  width: 48px;
   border-radius: 44px;
   background: var(--app-primary);
-  box-shadow: var(--app-shadow-light);
+  border: 4px solid var(--app-white);
 
   @media (prefers-color-scheme: dark) {
     background: var(--app-white);
-    border: 2px solid var(--app-dark-contrast);
+    border: 4px solid var(--app-light-contrast);
+  }
+
+  &:active {
+    transition: 140ms ease-in-out;
+    transform: scale(0.94) !important;
   }
 
   &:after {
@@ -74,7 +97,7 @@ const props = defineProps({
     font-weight: bolder;
 
     @media (prefers-color-scheme: dark) {
-     color: var(--app-primary);
+      color: var(--app-primary);
     }
   }
 
