bd350327a06e1e5f0366d1358f27ee7df5501893
Frédéric Camblor <fcamblor@gmail.com>
2024-04-25 17:27:06 +0200

    handling public token pattern matching within ensureEventBasedTokenPredicateIsValid() utility method

--- cloud/functions/src/functions/http/api/route-access.ts
+++ cloud/functions/src/functions/http/api/route-access.ts
@@ -5,7 +5,7 @@ import {
   getRoomStatsContributorValidToken
 } from "../../firestore/services/publicTokens-utils";
 import {match, P} from "ts-pattern";
-import {ConferenceDescriptor} from "../../../../../../shared/conference-descriptor.firestore";
+import {PublicToken} from "../../../../../../shared/public-tokens";
 
 
 export function publicEndpoint() {
@@ -21,7 +21,7 @@ export function ensureHasSuperAdminToken() {
   }
 }
 
-function ensureEventBasedTokenPredicateIsValid(predicate: (token: string, eventDescriptor: ConferenceDescriptor) => Promise<void>) {
+function ensureEventBasedTokenPredicateIsValid<T extends PublicToken>(publicTokenResolver: (token: string) => Promise<T>) {
   return async (pathParams: { eventId: string }, queryParams: { token: string }) => {
     const token = queryParams.token;
 
@@ -31,54 +31,33 @@ function ensureEventBasedTokenPredicateIsValid(predicate: (token: string, eventD
       throw new Error(`No event found with id ${pathParams.eventId}`)
     }
 
-    await predicate(token, eventDescriptor);
+    const matchingPublicToken = await publicTokenResolver(token);
+
+    const validationErrorMessage = match(matchingPublicToken as PublicToken)
+      .with({ eventFamilies: P.array(P.string) }, ({ eventFamilies: tokenEventFamilies }) => {
+        if(!eventDescriptor.eventFamily || !tokenEventFamilies.includes(eventDescriptor.eventFamily)) {
+          return `Provided event family-based token doesn't match with event ${eventDescriptor.id} family: [${eventDescriptor.eventFamily}]`;
+        }
+        return undefined;
+      }).with({ eventNames: P.array(P.string) }, ({ eventNames: tokenEventNames }) => {
+        if(!eventDescriptor.eventName || !tokenEventNames.includes(eventDescriptor.eventName)) {
+          return `Provided event-based token doesn't match with event ${eventDescriptor.id} name: [${eventDescriptor.eventName}]`
+        }
+        return undefined;
+      }).exhaustive();
+
+    if(validationErrorMessage) {
+      throw new Error(validationErrorMessage);
+    }
 
     return eventDescriptor;
   }
 }
 
 export function ensureHasFamilyOrEventOrganizerToken() {
-  return ensureEventBasedTokenPredicateIsValid(async (token, eventDescriptor) => {
-    const familyOrEventOrganizerToken = await getFamilyOrEventOrganizerToken(token);
-
-    const validationErrorMessage = match(familyOrEventOrganizerToken)
-      .with({ type: "FamilyOrganizerToken"}, (familyOrganizerToken) => {
-        if(!eventDescriptor.eventFamily || !familyOrganizerToken.eventFamilies.includes(eventDescriptor.eventFamily)) {
-          return `Provided family organizer token doesn't match with event ${eventDescriptor.id} family: [${eventDescriptor.eventFamily}]`;
-        }
-        return undefined;
-      }).with({ type: "EventOrganizerToken" }, (eventOrganizerToken) => {
-        if(!eventDescriptor.eventName || !eventOrganizerToken.eventNames.includes(eventDescriptor.eventName)) {
-          return `Provided event organizer token doesn't match with event ${eventDescriptor.id} name: [${eventDescriptor.eventName}]`
-        }
-        return undefined;
-      }).exhaustive();
-
-    if(validationErrorMessage) {
-      throw new Error(validationErrorMessage);
-    }
-  })
+  return ensureEventBasedTokenPredicateIsValid((token) => getFamilyOrEventOrganizerToken(token))
 }
 
 export function ensureHasRoomStatsContributorValidToken() {
-  return ensureEventBasedTokenPredicateIsValid(async (token, eventDescriptor) => {
-    const roomStatContributorToken = await getRoomStatsContributorValidToken(token);
-
-    const validationErrorMessage = match(roomStatContributorToken)
-      .with({ eventFamilies: P.array(P.string) }, ({ eventFamilies: tokenEventFamilies }) => {
-        if(!eventDescriptor.eventFamily || !tokenEventFamilies.includes(eventDescriptor.eventFamily)) {
-          return `Provided token doesn't match with event ${eventDescriptor.id} family: [${eventDescriptor.eventFamily}]`;
-        }
-        return undefined;
-      }).with({ eventNames: P.array(P.string) }, ({ eventNames: tokenEventNames }) => {
-        if(!eventDescriptor.eventName || !tokenEventNames.includes(eventDescriptor.eventName)) {
-          return `Provided token doesn't match with event ${eventDescriptor.id} name: [${eventDescriptor.eventName}]`
-        }
-        return undefined;
-      }).exhaustive();
-
-    if(validationErrorMessage) {
-      throw new Error(validationErrorMessage);
-    }
-  })
+  return ensureEventBasedTokenPredicateIsValid((token) => getRoomStatsContributorValidToken(token))
 }
